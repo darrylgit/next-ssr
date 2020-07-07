9a1428e728f5d6096783730ece54868d3a641e8c
darrylgit <darrylsmithup@gmail.com>
2020-07-07 13:48:24 -0400

    Add about page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6c4518e372911a76380b";
+/******/ 	var hotCurrentHash = "cf5945e3873489e153b7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
     <h1>SSR Magician</h1>
+    <Link href='/about'>
+      <button>About</button>
+    </Link>
   </div>
 );
 
