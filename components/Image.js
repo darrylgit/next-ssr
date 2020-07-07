60a96a21b741c294770ba52e53b8ef38ea4e065e
darrylgit <darrylsmithup@gmail.com>
2020-07-07 13:56:28 -0400

    Add Image component

---
+++ components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+  <img src='https://assets.petco.com/petco/image/upload/f_auto,q_auto,t_ProductDetail-large/253537-Center-4' />
+);
+
+export default Image;
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
+import Image from '../components/Image';
 
 const About = () => {
   return (
     <div style={{ fontSize: '20px', color: 'blue' }}>
       <h1>About</h1>
+      <Image />
       <Link href='/'>
-        <button>Back</button>
+        <button style={{ display: 'block' }}>Back</button>
       </Link>
       <p>I was a magician once</p>
     </div>
